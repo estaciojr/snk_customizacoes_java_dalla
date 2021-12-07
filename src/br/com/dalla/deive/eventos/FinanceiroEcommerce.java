5d705873207f2ae98ea5cbf541f5f11fee795a2b
Deive Altoé <deivealtoe@gmail.com>
2021-12-07 17:55:28 -0300

    Alterado para utilizar utilitario da biblioteca do sankhya

--- src/br/com/dalla/deive/eventos/FinanceiroEcommerce.java
+++ src/br/com/dalla/deive/eventos/FinanceiroEcommerce.java
@@ -1,31 +1,29 @@
 package br.com.dalla.deive.eventos;
 
-import java.util.Collection;
-import java.util.Iterator;
+import java.math.BigDecimal;
 
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
-import br.com.sankhya.jape.EntityFacade;
-import br.com.sankhya.jape.bmp.PersistentLocalEntity;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
-import br.com.sankhya.jape.util.FinderWrapper;
 import br.com.sankhya.jape.vo.DynamicVO;
 import br.com.sankhya.jape.wrapper.JapeFactory;
 import br.com.sankhya.jape.wrapper.JapeWrapper;
-import br.com.sankhya.modelcore.util.EntityFacadeFactory;
+import br.com.sankhya.modelcore.comercial.CentralFinanceiro;
 
 public class FinanceiroEcommerce implements EventoProgramavelJava {
 
 	public void beforeInsert(PersistenceEvent event) throws Exception { }
 	
 	public void afterInsert(PersistenceEvent event) throws Exception {
-		this.inicio(event);
+		//this.tgfcab(event);
+		this.tgffin(event);
 	}
 	
 	public void beforeUpdate(PersistenceEvent event) throws Exception { }
 	
 	public void afterUpdate(PersistenceEvent event) throws Exception {
-		this.inicio(event);
+		//this.tgfcab(event);
+		//this.tgffin(event);
 	}
 	
 	public void beforeDelete(PersistenceEvent event) throws Exception { }
@@ -34,7 +32,32 @@ public class FinanceiroEcommerce implements EventoProgramavelJava {
 
 	public void beforeCommit(TransactionContext event) throws Exception { }
 	
-	public void inicio(PersistenceEvent event) throws Exception {
+	public void tgffin(PersistenceEvent event) throws Exception {
+		DynamicVO tituloAtual = (DynamicVO) event.getVo();
+		
+		int nroUnicoAtual = tituloAtual.asInt("NUNOTA");
+		
+		DynamicVO pedidoAtualVO = this.getTgfcab(nroUnicoAtual);
+		
+		if (pedidoAtualVO != null) {
+			int nroUnicoOrigem = pedidoAtualVO.asInt("AD_NUNOTAORIG");
+			
+			DynamicVO pedidoOrigemVO = this.getTgfcab(nroUnicoOrigem);
+			
+			if (pedidoOrigemVO != null) {
+				int codEmpOrig = pedidoOrigemVO.asInt("CODEMP");
+				int codTipOperOrig = pedidoOrigemVO.asInt("CODTIPOPER");
+				String nuPedidoVtex = pedidoOrigemVO.asString("AD_PEDIDOECOM");
+				
+				if (codEmpOrig == 9 && codTipOperOrig == 1009 && nuPedidoVtex != null && nroUnicoAtual != nroUnicoOrigem) {
+					CentralFinanceiro centralFinanceiro = new CentralFinanceiro();
+					centralFinanceiro.excluiFinanceiro(BigDecimal.valueOf(nroUnicoAtual));
+				}
+			}
+		}
+	}
+	
+	public void tgfcab(PersistenceEvent event) throws Exception {
 		DynamicVO pedidoAtualVO = (DynamicVO) event.getVo();
 		int nroUnicoPedAtual = pedidoAtualVO.asInt("NUNOTA");
 		
@@ -43,7 +66,10 @@ public class FinanceiroEcommerce implements EventoProgramavelJava {
 		DynamicVO pedidoOrigemVO = this.getTgfcab(nroUnicoPedOrigem);
 		
 		if (pedidoOrigemVO != null) {
-			int codEmpOrig = pedidoOrigemVO.asInt("CODEMP");
+			CentralFinanceiro centralFinanceiro = new CentralFinanceiro();
+			centralFinanceiro.excluiFinanceiro(BigDecimal.valueOf(nroUnicoPedAtual));
+			
+			/*int codEmpOrig = pedidoOrigemVO.asInt("CODEMP");
 			int codTipOperOrig = pedidoOrigemVO.asInt("CODTIPOPER");
 			String nuPedidoVtex = pedidoOrigemVO.asString("AD_PEDIDOECOM");
 			
@@ -56,7 +82,7 @@ public class FinanceiroEcommerce implements EventoProgramavelJava {
 					PersistentLocalEntity persistentLocalEntity = (PersistentLocalEntity) titulosIterator.next();
 					persistentLocalEntity.remove();
 				}
-			}
+			}*/
 		}
 	}
 	
