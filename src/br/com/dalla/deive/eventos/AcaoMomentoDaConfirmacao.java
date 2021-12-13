64823b59817cabb24205c8ee844c706f611de395
Deive Altoé <deivealtoe@gmail.com>
2021-12-13 18:26:34 -0300

    Efetuado novos testes

--- src/br/com/dalla/deive/eventos/AcaoMomentoDaConfirmacao.java
+++ src/br/com/dalla/deive/eventos/AcaoMomentoDaConfirmacao.java
@@ -1,6 +1,32 @@
 package br.com.dalla.deive.eventos;
 
-public class AcaoMomentoDaConfirmacao {
-	
-}
+import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
+import br.com.sankhya.jape.event.PersistenceEvent;
+import br.com.sankhya.jape.event.TransactionContext;
+import br.com.sankhya.jape.vo.DynamicVO;
+import br.com.sankhya.modelcore.comercial.CentralFaturamento;
+import br.com.sankhya.modelcore.comercial.ConfirmacaoNotaHelper;
 
+public class AcaoMomentoDaConfirmacao implements EventoProgramavelJava {
+
+	public void beforeInsert(PersistenceEvent event) throws Exception { }
+
+	public void afterInsert(PersistenceEvent event) throws Exception { }
+
+	public void beforeUpdate(PersistenceEvent event) throws Exception { }
+
+	public void afterUpdate(PersistenceEvent event) throws Exception {
+		DynamicVO pedidoAtual = (DynamicVO) event.getVo();
+		if (ConfirmacaoNotaHelper.verificaConfComercial(pedidoAtual.asBigDecimal("NUNOTA"))) {
+			
+		}
+		
+	}
+
+	public void beforeDelete(PersistenceEvent event) throws Exception { }
+
+	public void afterDelete(PersistenceEvent event) throws Exception { }
+
+	public void beforeCommit(TransactionContext event) throws Exception { }
+
+}
---
+++ src/br/com/dalla/deive/util/Teste.java
@@ -0,0 +1,11 @@
+package br.com.dalla.deive.util;
+
+public class Teste {
+	
+	private String teste;
+	
+	public Teste(String teste) {
+		this.teste = teste;
+	}
+	
+}
