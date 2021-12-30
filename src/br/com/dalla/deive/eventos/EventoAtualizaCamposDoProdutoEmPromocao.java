8d7a3cb525ffb9a7ddc8bd93eab7f1c859bd552b
Deive Altoé <deivealtoe@gmail.com>
2021-12-30 16:59:36 -0300

    Classe renomeada e alterado o evento para funcionar tanto no afterInsert quanto no beforeUpdate

--- src/br/com/dalla/deive/eventos/EventoAtualizaCamposDoProdutoEmPromocao.java
+++ src/br/com/dalla/deive/eventos/EventoAtualizaCamposDoProdutoEmPromocao.java
@@ -4,22 +4,19 @@ import br.com.dalla.deive.util.ProdutoDaPromocao;
 import br.com.sankhya.extensions.eventoprogramavel.EventoProgramavelJava;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.event.TransactionContext;
-import br.com.sankhya.jape.vo.DynamicVO;
 
-public class EventoAtualizaPrecoTabelaProdutoPromocao implements EventoProgramavelJava {
+public class EventoAtualizaCamposDoProdutoEmPromocao implements EventoProgramavelJava {
 
 	@Override
 	public void afterDelete(PersistenceEvent arg0) throws Exception { }
 
 	@Override
 	public void afterInsert(PersistenceEvent persistenceEvent) throws Exception {
-		this.inicio(persistenceEvent);
+		this.inicio(persistenceEvent, "afterInsert");
 	}
 
 	@Override
-	public void afterUpdate(PersistenceEvent persistenceEvent) throws Exception {
-		this.inicio(persistenceEvent);
-	}
+	public void afterUpdate(PersistenceEvent persistenceEvent) throws Exception { }
 
 	@Override
 	public void beforeCommit(TransactionContext arg0) throws Exception { }
@@ -31,12 +28,12 @@ public class EventoAtualizaPrecoTabelaProdutoPromocao implements EventoProgramav
 	public void beforeInsert(PersistenceEvent persistenceEvent) throws Exception { }
 
 	@Override
-	public void beforeUpdate(PersistenceEvent arg0) throws Exception { }
+	public void beforeUpdate(PersistenceEvent persistenceEvent) throws Exception {
+		this.inicio(persistenceEvent, "beforeUpdate");
+	}
 	
-	private void inicio(PersistenceEvent persistenceEvent) throws Exception {
-		DynamicVO produtoDaPromocaoVO = (DynamicVO) persistenceEvent.getVo();
-		
-		ProdutoDaPromocao produtoDaPromocao = new ProdutoDaPromocao(produtoDaPromocaoVO, persistenceEvent);
+	private void inicio(PersistenceEvent persistenceEvent, String ondeEstaOEvento) throws Exception {
+		ProdutoDaPromocao produtoDaPromocao = new ProdutoDaPromocao(persistenceEvent, ondeEstaOEvento);
 		
 		if (produtoDaPromocao.setCabecalhoDaPromocaoVO()) {
 			if (produtoDaPromocao.setTabelaDePrecoVO()) {
--- src/br/com/dalla/deive/util/ProdutoDaPromocao.java
+++ src/br/com/dalla/deive/util/ProdutoDaPromocao.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import br.com.sankhya.jape.bmp.PersistentLocalEntity;
+import br.com.sankhya.jape.event.ModifingFields;
 import br.com.sankhya.jape.event.PersistenceEvent;
 import br.com.sankhya.jape.vo.DynamicVO;
 import br.com.sankhya.jape.vo.EntityVO;
@@ -15,9 +16,13 @@ public class ProdutoDaPromocao {
 	private DynamicVO produtoDaPromocao = null;
 	private DynamicVO cabecalhoDaPromocaoVO = null;
 	private DynamicVO tabelaDePrecoVO = null;
+	private PersistenceEvent persistenceEvent;
+	private String ondeEstaOEvento;
 	
-	public ProdutoDaPromocao(DynamicVO vo, PersistenceEvent persistenceEvent) throws Exception {
-		this.produtoDaPromocao = vo;
+	public ProdutoDaPromocao(PersistenceEvent persistenceEvent, String ondeEstaOEvento) throws Exception {
+		this.produtoDaPromocao = (DynamicVO) persistenceEvent.getVo();
+		this.persistenceEvent = persistenceEvent;
+		this.ondeEstaOEvento = ondeEstaOEvento;
 	}
 	
 	public boolean setCabecalhoDaPromocaoVO() throws Exception {
@@ -42,17 +47,17 @@ public class ProdutoDaPromocao {
 	
 	private BigDecimal getPrecoDaTabela() throws Exception {
 		return ComercialUtils.obtemPreco2(
-				 this.cabecalhoDaPromocaoVO.asBigDecimalOrZero("NUTAB"),
-		 		 this.produtoDaPromocao.asBigDecimalOrZero("CODPROD"),
-		 		 this.tabelaDePrecoVO.asTimestamp("DTVIGOR")
-			 );
+			this.cabecalhoDaPromocaoVO.asBigDecimalOrZero("NUTAB"),
+	 		this.produtoDaPromocao.asBigDecimalOrZero("CODPROD"),
+	 		this.tabelaDePrecoVO.asTimestamp("DTVIGOR")
+		);
 	}
 	
 	private BigDecimal getCustoGerencial() throws Exception {
 		return ComercialUtils.getUltimoCusto(this.produtoDaPromocao.asBigDecimalOrZero("CODPROD"), BigDecimal.valueOf(1), BigDecimal.valueOf(0), "", "CUSGER");
 	}
 	
-	private BigDecimal getPercentualDeDesconto() throws Exception {
+	private BigDecimal getPercentualDeDescontoCalculadoPeloPeloPrecoPromocional() throws Exception {
 		double precoPromocional = this.produtoDaPromocao.asBigDecimalOrZero("PRECOPROM").doubleValue();
 		double precoDeTabela = this.getPrecoDaTabela().doubleValue();
 		
@@ -61,6 +66,17 @@ public class ProdutoDaPromocao {
 		percentualDeDesconto = percentualDeDesconto.setScale(2, RoundingMode.HALF_EVEN);
 		
 		return percentualDeDesconto;
+	}
+	
+	private BigDecimal getValorPromocionalCalculadoPelaPorcentagemDeDesconto() throws Exception {
+		double percentualDeDesconto = this.produtoDaPromocao.asBigDecimalOrZero("PERCDESC").doubleValue();
+		double precoDeTabela = this.getPrecoDaTabela().doubleValue();
+		
+		BigDecimal valorPromocional = BigDecimal.valueOf(precoDeTabela * percentualDeDesconto / 100);
+		
+		valorPromocional = valorPromocional.setScale(2, RoundingMode.HALF_EVEN);
+		
+		return valorPromocional;
 	}
 	
 	private boolean valorDoDescontoEhMaiorQueValorDeVenda() throws Exception {
@@ -86,7 +102,7 @@ public class ProdutoDaPromocao {
 			+ ". Produto=" + this.produtoDaPromocao.asBigDecimalOrZero("CODPROD")
 			+ ". Preco na tabela=" + this.getPrecoDaTabela()
 			+ ". Preco promocional=" + this.produtoDaPromocao.asBigDecimalOrZero("PRECOPROM")
-			+ ". Percentual de desconto=" + this.getPercentualDeDesconto()
+			+ ". Percentual de desconto=" + this.getPercentualDeDescontoCalculadoPeloPeloPrecoPromocional()
 			+ ". Custo gerencial=" + this.getCustoGerencial()
 		);
 		
@@ -106,10 +122,70 @@ public class ProdutoDaPromocao {
 			);
 		}
 		
+		/* Quando o evento de afterInsert é executado, é feito modificação no campo
+		 * PERCDESC, que por consequência o evento tenta modificar o campo PRECOPROM, ele entra
+		 * na cláusula do beforeUpdate. Dessa forma recalculando o valor da porcentagem de desconto.
+		 * Essa variável faz a identificação que os dados estão sendo inseridos.
+		 * Quando o evento entra na cláusula do beforeUpdate através do afterInsert, essa alteração
+		 * da porcentagem de desconto é ignorada.
+		 * Exemplo:
+		 *  	Inserindo o produto 1260, com preço de venda 10x sendo 23,45.
+		 *  	É inserido a porcentagem de 10% de desconto.
+		 *  	O que dá 3,34 no valor do desconto.
+		 *  	Quando o valor promocional/valor do desconto é modificado o evento
+		 *  	entra no beforeUpdate, calculando a porcentagem de desconto usando como base o valor
+		 *  	de 3,34. O que equivale a 9,98%. Dessa forma a porcentagem de desconto ficava sendo 9,98 ao invés de 10
+		 */
+		String estahSendoInserido = "";
+		if (this.produtoDaPromocao.asString("ESTAH_SENDO_INSERIDO") == null) {
+			estahSendoInserido = "S";
+		} else {
+			estahSendoInserido = this.produtoDaPromocao.asString("ESTAH_SENDO_INSERIDO");
+		}
+		
+		if (ondeEstaOEvento.equals("afterInsert")) {
+			this.eventoAfterInsert();
+		} else if (ondeEstaOEvento.equals("beforeUpdate") && !estahSendoInserido.equals("S")) {
+			this.eventoBeforeUpdate();
+		}
+	}
+	
+	private void eventoAfterInsert() throws Exception {
 		PersistentLocalEntity persistentLocalEntity = EntityFacadeFactory.getDWFFacade().findEntityByPrimaryKey("AD_TGFDESC", new Object[] { this.produtoDaPromocao.asBigDecimalOrZero("CODGRUPODESC"), this.produtoDaPromocao.asBigDecimalOrZero("SEQUENCIA") });
 		DynamicVO dynamicVO = (DynamicVO) persistentLocalEntity.getValueObject();
 		dynamicVO.setProperty("PRECOTAB", this.getPrecoDaTabela());
-		dynamicVO.setProperty("PERCDESC", this.getPercentualDeDesconto());
+		dynamicVO.setProperty("ESTAH_SENDO_INSERIDO", "S");
+		
+		if (this.produtoDaPromocao.asBigDecimalOrZero("PERCDESC").doubleValue() != 0) {
+			dynamicVO.setProperty("PRECOPROM", this.getValorPromocionalCalculadoPelaPorcentagemDeDesconto());
+		} else {
+			dynamicVO.setProperty("PERCDESC", this.getPercentualDeDescontoCalculadoPeloPeloPrecoPromocional());
+		}
+		
+		persistentLocalEntity.setValueObject((EntityVO) dynamicVO);
+		
+		this.marcarNaoEstaSendoInserido();
+	}
+	
+	private void eventoBeforeUpdate() throws Exception {
+		this.produtoDaPromocao.setProperty("PRECOTAB", this.getPrecoDaTabela());
+		
+		ModifingFields modifingFields = this.persistenceEvent.getModifingFields();
+		if (modifingFields.isModifing("PERCDESC")) {
+			System.out.println("Está sendo modificado o campo PERCDESC!!!!");
+			this.produtoDaPromocao.setProperty("PRECOPROM", this.getValorPromocionalCalculadoPelaPorcentagemDeDesconto());
+		} else if (modifingFields.isModifing("PRECOPROM")) {
+			System.out.println("Está sendo modificado o campo PRECOPROM!!!!");
+			this.produtoDaPromocao.setProperty("PERCDESC", this.getPercentualDeDescontoCalculadoPeloPeloPrecoPromocional());
+		}
+	}
+	
+	private void marcarNaoEstaSendoInserido() throws Exception {
+		PersistentLocalEntity persistentLocalEntity = EntityFacadeFactory.getDWFFacade().findEntityByPrimaryKey("AD_TGFDESC", new Object[] { this.produtoDaPromocao.asBigDecimalOrZero("CODGRUPODESC"), this.produtoDaPromocao.asBigDecimalOrZero("SEQUENCIA") });
+		DynamicVO dynamicVO = (DynamicVO) persistentLocalEntity.getValueObject();
+		dynamicVO.setProperty("PRECOTAB", this.getPrecoDaTabela());
+		dynamicVO.setProperty("ESTAH_SENDO_INSERIDO", "N");
+		
 		persistentLocalEntity.setValueObject((EntityVO) dynamicVO);
 	}
 
